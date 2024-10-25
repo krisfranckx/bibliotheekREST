09a448de5dbc38cc0223baeb461ac2a793d6fdb6
krisfranckx <krisfranckx@hotmail.com>
2024-10-25 21:26:43 +0200

    Second commit

---
+++ src/main/java/be/atheneumboom/bibliotheek/controller/ConfirmationController.java
@@ -0,0 +1,43 @@
+package be.atheneumboom.bibliotheek.controller;
+
+import be.atheneumboom.bibliotheek.config.Settings;
+import be.atheneumboom.bibliotheek.model.token.ConfirmationTokenService;
+import be.atheneumboom.bibliotheek.service.UserService;
+import be.atheneumboom.bibliotheek.service.impl.RegistrationService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("api/registration")
+@AllArgsConstructor
+public class ConfirmationController {
+    @Autowired
+    private final RegistrationService registrationService;
+    private final UserService userService;
+            ;
+
+    private final ConfirmationTokenService confirmationTokenService;
+    @GetMapping("/confirm")
+    public RedirectView confirm(@RequestParam("token") String token) {
+        //Long userId = confirmationTokenService.getToken(token).get().getUser().getId();
+        try{
+            registrationService.confirmToken(token);
+
+        }catch (IllegalStateException e){
+            Long userId = confirmationTokenService.getToken(token).get().getUser().getId();
+            registrationService.sendNewConfirmationMail(userService.getRealUser(userId));
+            RedirectView redirectView = new RedirectView();
+            redirectView.setUrl(Settings.BASE_URL_FRONT+"/expired");
+            return redirectView;
+        }
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(Settings.BASE_URL_FRONT+"/confirmed");
+        return redirectView;
+    }
+
+
+    
+}
